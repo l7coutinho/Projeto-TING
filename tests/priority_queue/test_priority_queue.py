e6f9c674287b613118b0e02af93c93efcd5fca1a
Lucas Coutinho <l7coutinho.dev@gmail.com>
2024-04-15 12:15:42 -0300

    add test_priority_queue, req6

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,25 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    pq = PriorityQueue()
+
+    pq.enqueue({"name_file": "a", "qtd_linhas": 10})
+    pq.enqueue({"name_file": "b", "qtd_linhas": 3})
+    pq.enqueue({"name_file": "c", "qtd_linhas": 9})
+
+    assert len(pq) == 3
+
+    search_pq = pq.search(1)
+    assert search_pq["name_file"] == "a"
+    assert search_pq["qtd_linhas"] == 10
+
+    pq.dequeue()
+    assert len(pq) == 2
+
+    pq.dequeue()
+    assert len(pq) == 1
+
+    with pytest.raises(IndexError, match="Índice Inválido ou Inexistente"):
+        pq.search(999)
