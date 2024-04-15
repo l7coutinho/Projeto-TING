871f92bc72612d068ab6891855796be68f2097ea
Lucas Coutinho <l7coutinho.dev@gmail.com>
2024-04-15 11:30:48 -0300

    add func process, req3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    list = txt_importer(path_file)
+
+    for index in instance._data:
+        if index['nome_do_arquivo'] == path_file:
+            return
+
+    file = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(list),
+        'linhas_do_arquivo': list
+    }
+
+    instance.enqueue(file)
+    print(file, file=sys.stdout)
 
 
 def remove(instance):
