402d73b10f23d02532213efa90217f7ec228ca03
Lucas Coutinho <l7coutinho.dev@gmail.com>
2024-04-15 11:01:41 -0300

    add function txt_importer, req2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,12 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+    try:
+        with open(path_file, "r") as file:
+            file_content = file.read().split("\n")
+            return file_content
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
