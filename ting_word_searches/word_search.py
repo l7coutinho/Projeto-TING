1ae168edf530325e7d92c0c615a5d9747d04fbc0
Lucas Coutinho <l7coutinho.dev@gmail.com>
2024-04-15 15:17:14 -0300

    add search_by_word func, req8

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -19,4 +19,21 @@ def exists_word(word, instance):
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    list = []
+
+    for data in instance._data:
+        obj_word = {
+            "palavra": word,
+            "arquivo": data["nome_do_arquivo"],
+            "ocorrencias": [],
+        }
+        for line_number, line in enumerate(data["linhas_do_arquivo"]):
+            if word.lower() in line.lower():
+                obj_word["ocorrencias"].append(
+                    {"linha": line_number + 1, "conteudo": line}
+                )
+
+        if len(obj_word["ocorrencias"]) > 0:
+            list.append(obj_word)
+
+    return list
